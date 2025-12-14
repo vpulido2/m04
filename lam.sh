299ca18af65d3022f86504f477352fe13a5d344a
vpulido2 <32706662+vpulido2@users.noreply.github.com>
2025-12-14 18:01:28 +0100

    Update PHP installation to version 8.1

--- lam.sh
+++ lam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
-sudo apt -y install apache2 php php-cgi libapache2-mod-php php-mbstring php-common php-pear
-sudo a2enconf php7.4-cgi
+sudo apt -y install apache2 libapache2-mod-php8.1 php8.1 php8.1-mysql php8.1-cli php8.1-common php8.1-xml php8.1-curl php8.1-mbstring
+sudo a2enconf php8.1
 sudo systemctl reload apache2
 wget https://github.com/LDAPAccountManager/lam/releases/download/8.5/ldap-account-manager_8.5-1_all.deb
 sudo apt install ./ldap-account-manager_8.5-1_all.deb -y
